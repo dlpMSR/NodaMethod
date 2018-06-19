6b6954b48e9a4580869518d9b09e67b96f6f583f
DLPMSR-FUJI\Zechs <pntsec_msr@outlook.com>
2018-06-19 17:12:54 +0900

    データベースのファイル作る/フォルダ中の画像のラベル全部登録する　ようになった

--- dbmaker.py
+++ dbmaker.py
@@ -1,21 +1,46 @@
-import csv
+import os
+import sys
+import glob
 import sqlite3
 
-csvpath = './label1.csv'
-conn = sqlite3.connect('./label.db')
-c = conn.cursor()
+labelpath = './label_M_g.db'
 
-create_table = '''create table images(filename, label)'''
-c.execute(create_table)
-
-
-
-with open(csvpath, 'r', encoding='utf-8') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        sql = 'insert into images (filename, label) values (?,?)'
-        image = (row[0], row[1])
-        c.execute(sql, image)
+def create_dbfile():
+    conn = sqlite3.connect(labelpath)
+    c = conn.cursor()
+    create_table = '''create table images(filename, label)'''
+    c.execute(create_table)
     conn.commit()
+    conn.close()
 
-conn.close()
+
+def insert_all_images(path, label):
+    targets = glob.glob(os.path.join(path, '*.jpg'))
+    num_of_images = len(targets)
+    print("{}枚の画像があります．．．".format(num_of_images))
+
+    conn = sqlite3.connect(labelpath)
+    c = conn.cursor()
+
+    for image in targets:
+        filename = os.path.basename(image)
+        label = int(label)
+        sql = 'insert into images (filename, label) values (?,?)'
+        image = (filename, label)
+        c.execute(sql, image)
+    
+    conn.commit()
+    conn.close()
+
+
+def main(arg1, arg2):
+    arg = sys.argv
+    path = arg1
+    label = arg2
+    create_dbfile()
+    #insert_all_images(path, label)
+
+
+if __name__ == '__main__':
+    arg = sys.argv
+    main(arg[1], arg[2])
